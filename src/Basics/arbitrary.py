b7a7fab5ae04a361d4f6b7da75526760f8ef8d2e
BhaveshDotCom <mail.bhavesh.in@gmail.com>
2026-04-16 20:10:25 +0530

    feat(basics): add **kwargs example function

--- src/Basics/arbitrary.py
+++ src/Basics/arbitrary.py
@@ -2,9 +2,21 @@
 # **kwargs - pass multiple keywords arguments, dict
 
 def sum(*args):
+    print(type(args))
     total = 0
     for arg in args:
         total+=arg
     return total
 
 print(sum(1,2,3,4,5))
+
+def address(**kwargs):
+    print(type(kwargs))
+    for key, value in kwargs.items():
+        print( f"{key}: {value}" )
+
+
+print(address(
+    street= "Park Avenue",
+    pincode="123455"
+))
